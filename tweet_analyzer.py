bb0736be6e90c329fcdb4cdf012612620481fb3d
Wisal Mukhtiar <63037195+Wisal-Mukhtiar@users.noreply.github.com>
2022-08-18 11:11:26 +0500

    adding functionality to analyzer

--- tweet_analyzer.py
+++ tweet_analyzer.py
@@ -7,6 +7,9 @@ class TweetAnalyzer():
     """
 
     def tweets_to_dataframe(self, tweets):
-            df = pd.DataFrame(
-                data=[tweet.text for tweet in tweets], columns=['Tweets'])
+        df = pd.DataFrame(
+            data=[tweet.text for tweet in tweets], columns=['Tweets'])
+        # after the dataFrame is created we can add other attributes of the tweets to it like this
+        df['id'] = np.array([tweet.id])
+
         return df
