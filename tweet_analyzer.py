16d312606007b1644c983f332fa75b76bf051563
Wisal Mukhtiar <63037195+Wisal-Mukhtiar@users.noreply.github.com>
2022-08-18 11:12:55 +0500

    Revert "adding functionality to analyzer"
    This reverts commit bb0736be6e90c329fcdb4cdf012612620481fb3d.

--- tweet_analyzer.py
+++ tweet_analyzer.py
@@ -7,9 +7,6 @@ class TweetAnalyzer():
     """
 
     def tweets_to_dataframe(self, tweets):
-        df = pd.DataFrame(
-            data=[tweet.text for tweet in tweets], columns=['Tweets'])
-        # after the dataFrame is created we can add other attributes of the tweets to it like this
-        df['id'] = np.array([tweet.id])
-
+            df = pd.DataFrame(
+                data=[tweet.text for tweet in tweets], columns=['Tweets'])
         return df
