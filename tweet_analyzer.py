859c6159c953fb67d908c7693029764a8a2836b0
Wisal Mukhtiar <63037195+Wisal-Mukhtiar@users.noreply.github.com>
2022-08-18 11:08:14 +0500

    module contain functionality  of tweets analysis

---
+++ tweet_analyzer.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+
+class TweetAnalyzer():
+    """
+    Functionality for analyzing and categorizing tweets
+    """
+
+    def tweets_to_dataframe(self, tweets):
+            df = pd.DataFrame(
+                data=[tweet.text for tweet in tweets], columns=['Tweets'])
+        return df
