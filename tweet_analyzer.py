6c934ae61f18f2a17ebc7c018dec7f0e43fd9fba
Wisal Mukhtiar <63037195+Wisal-Mukhtiar@users.noreply.github.com>
2022-08-19 16:30:32 +0500

    feat: added some features to the returned dataFrame

--- tweet_analyzer.py
+++ tweet_analyzer.py
@@ -12,5 +12,10 @@ class TweetAnalyzer():
             data=[tweet.text for tweet in tweets], columns=['Tweets'])
         # can add other attributes of tweets to the df as follows once the df is created
         df['id'] = np.array([tweet.id for tweet in tweets])
+        df['len'] = np.array([len(tweet.text) for tweet in tweets])
+        df['date'] = np.array([tweet.created_at for tweet in tweets])
+        df['source'] = np.array([tweet.source for tweet in tweets])
+        df['likes'] = np.array([tweet.favorite_count for tweet in tweets])
+        df['retweets'] = np.array([tweet.retweet_count for tweet in tweets])
 
         return df
