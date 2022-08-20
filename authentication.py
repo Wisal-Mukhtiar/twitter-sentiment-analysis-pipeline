41bb7db1be4083bf691e9e6cf2646c92daf341ec
Wisal Mukhtiar <63037195+Wisal-Mukhtiar@users.noreply.github.com>
2022-08-20 13:08:55 +0500

    feat: get twitter client from TwitterAuthenticator

--- authentication.py
+++ authentication.py
@@ -8,9 +8,16 @@ class TwitterAuthenticator():
     class to authenticate the account
     """
 
+    def __init__(self):
+        self.auth = auth = OAuthHandler(twitter_credentials.CONSUMER_KEY,
+                                        twitter_credentials.CONSUMER_SECRET)
+        self.auth.set_access_token(twitter_credentials.ACCESS_TOKEN,
+                                   twitter_credentials.ACCESS_SECRET)
+
     def authenticate_twitter_app(self):
-        auth = OAuthHandler(twitter_credentials.CONSUMER_KEY,
-                            twitter_credentials.CONSUMER_SECRET)
-        auth.set_access_token(twitter_credentials.ACCESS_TOKEN,
-                              twitter_credentials.ACCESS_SECRET)
-        return auth
+        return self.auth
+
+    def get_twitter_client(self):
+        client = Client(consumer_key=self.auth.consumer_key, consumer_secret=self.auth.consumer_secret,
+                        access_token=self.auth.access_token, access_token_secret=self.auth.access_token_secret)
+        return client
