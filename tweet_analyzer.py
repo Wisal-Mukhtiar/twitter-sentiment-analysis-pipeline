49f4796e6046047281fe82c63e0bc0a5554f7e21
Wisal Mukhtiar <63037195+Wisal-Mukhtiar@users.noreply.github.com>
2022-08-27 11:24:57 +0500

    feat : sentiment analysis using textblob
    added the dataFrame column 'sentiment' that would show either tweet is positive negative or neutral

--- tweet_analyzer.py
+++ tweet_analyzer.py
@@ -1,11 +1,26 @@
 import pandas as pd
 import numpy as np
+from textblob import TextBlob
+import re
 
 
 class TweetAnalyzer():
     """
     Functionality for analyzing and categorizing tweets
     """
+
+    def clean_tweet(self, tweet):
+        return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
+
+    def analyze_sentiment(self, tweet):
+        analysis = TextBlob(self.clean_tweet(tweet))
+
+        if analysis.sentiment.polarity > 0:
+            return 1
+        elif analysis.sentiment.polarity == 0:
+            return 0
+        else:
+            return -1
 
     def tweets_to_dataframe(self, tweets):
         df = pd.DataFrame(
@@ -17,6 +32,7 @@ class TweetAnalyzer():
         df['source'] = np.array([tweet.source for tweet in tweets])
         df['retweets'] = np.array([tweet.retweet_count for tweet in tweets])
         df['likes'] = np.array([self.get_likes(tweet) for tweet in tweets])
+        df['sentiment'] = np.array([self.analyze_sentiment(tweet.text) for tweet in tweets])
         return df
 
     def get_likes(self, tweet):
